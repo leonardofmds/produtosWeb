856803371031e4d96a77fcfe369a0d46c5eab22f
Leonardo Menezes <leonardomenezes.dev@gmail.com>
2025-02-27 13:45:35 -0300

    Ajustes no formulario de cadastro e utilização da rota do endpoint pelo arquivo de configuração enviroments.ts

--- src/app/components/pages/cadastro-produtos/cadastro-produtos.component.ts
+++ src/app/components/pages/cadastro-produtos/cadastro-produtos.component.ts
@@ -1,68 +1,71 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { Validators, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { endpoints } from '../../../configurations/environments';
 
 @Component({
   selector: 'app-cadastro-produtos',
   imports: [
-    CommonModule,
-    FormsModule, //biblioteca de formularios
-    ReactiveFormsModule //biblioteca de formularios reativos
+    CommonModule, //biblioteca de componentes comuns
+    FormsModule, //biblioteca de formulários
+    ReactiveFormsModule //biblioteca de formulários reativos
   ],
   templateUrl: './cadastro-produtos.component.html',
   styleUrl: './cadastro-produtos.component.css'
 })
 export class CadastroProdutosComponent {
 
-  //declaracao de atributos
-  categorias : any=[]; //array de objetos
-  mensagem: string = '';
+  //atributos
+  categorias: any[] = []; //array de objetos JSON
+  mensagem: string = ''; //texto vazio
 
-  constructor(private http: HttpClient){
+  //construtor
+  constructor(private http: HttpClient) { }
 
+  //função executada ao iniciar o componente
+  ngOnInit() {
+
+    //fazendo uma requisição GET para a API
+    this.http.get(endpoints.consultar_categorias)
+      .subscribe({ //aguardando o retorno da API
+        next: (data) => { //capturando os dados obtidos
+          //atribuindo os dados obtidos à variável categorias
+          this.categorias = data as any[];
+        }
+      })
   }
 
-  //funcao executada ao iniciar o componente
-  ngOnInit(){
-    //fazendo uma rfequisicao GET para a API 
-    this.http.get('http://localhost:8081/api/categorias/consultar').subscribe({
-      next: (data) =>{
-       //atribuindo os dados obtidos a variavel categorias
-        this.categorias= data as any[];
-      }
-    })
-  }
-
-  //declarando o formulario
+  //declarando o formulário
   form = new FormGroup({
-    nome: new FormControl('',[
+    nome : new FormControl('', [
       Validators.required, Validators.minLength(8), Validators.maxLength(100)
     ]),
-    preco: new FormControl('',[
+    preco : new FormControl('', [
       Validators.required, Validators.min(0.01)
     ]),
-    quantidade: new FormControl('',[
+    quantidade : new FormControl('', [
       Validators.required, Validators.min(0)
     ]),
-    categoriaId: new FormControl('',[
+    categoriaId : new FormControl('', [
       Validators.required
-    ]),
-  })
+    ])
+  });
 
-  //funcao auxiliar para verificar se cada campo foi preenchido corretamente
-  get f(){
+  //função auxiliar para verificar se cada foi preenchido corretamente
+  get f() {
     return this.form.controls;
   }
 
-  //funcao para capturar o evento submit do formulario
-  onSubmit(){
-    //fazendo uma requisicao POST para o endpoint de cadastro de produtos da API
-    this.http.post("http://localhost:8081/api/produtos/cadastrar", this.form.value)
-      .subscribe({
-        next: (data: any) => {
-          this.mensagem = data.message;
-          this.form.reset();
+  //função para capturar o evento SUBMIT do formulário
+  onSubmit() {
+    
+    //fazendo uma requisição POST para o endpoint de cadastro de produtos
+    this.http.post(endpoints.cadastrar_produto, this.form.value)
+      .subscribe({ //aguardando o retorno da API
+        next: (data: any) => { //capturando os dados obtidos
+          this.mensagem = data.message; //capturando a mensagem de retorno
+          this.form.reset(); //limpando o formulário
         }
       })
   }
