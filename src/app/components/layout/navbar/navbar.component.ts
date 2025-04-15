4c552be9153e1e7cfbb34ba87dce20f1370e06ec
Leonardo Menezes <leonardomenezes.dev@gmail.com>
2025-04-15 11:03:44 -0300

    Navbar with user information

--- src/app/components/layout/navbar/navbar.component.ts
+++ src/app/components/layout/navbar/navbar.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
@@ -5,11 +6,27 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   imports: [
-    RouterLink
+    RouterLink,
+    CommonModule
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+  usuario: any = null;
+
+  ngOnInit(){
+    var data = sessionStorage.getItem('usuario') as string;
+
+    if(data!=null){
+      this.usuario = JSON.parse(data);
+    }
+  }
+
+  logout(){
+    sessionStorage.removeItem('usuario');
+    location.href='/';
+  }
 
 }
